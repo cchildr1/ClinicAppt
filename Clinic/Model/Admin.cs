9aec8105f538cefc0dde1e00747ec71189b850bb
Chris Childress <cchildre@westga.edu>
2019-03-06 14:37:50 -0500

    added classes for Employee, Nurse and Admin

---
+++ Clinic/Model/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic.Model
+{
+    /// <summary>
+    /// Represents Administrator of system
+    /// </summary>
+    public class Admin : Employee
+    {
+        public int AdminID { get; set; }
+    }
+}
