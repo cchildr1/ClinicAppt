5aa5e468f0a5e0bb99f6a835c383292ff0e8ee1a
Chris Childress <cchildre@westga.edu>
2019-03-06 16:01:44 -0500

    basic login query written

---
+++ Clinic/DAL/LoginDAL.cs
@@ -0,0 +1,41 @@
+﻿using Clinic.Model;
+using System.Data.SqlClient;
+
+namespace Clinic.DAL
+{
+    public static class Login
+    {
+        public static Employee LoginEmployee(string username, string password)
+        {
+            string selectStatement = "SELECT * from employees WHERE username = @username AND password = @password";
+
+            using (SqlConnection connection = ClinicDBConnection.GetConnection())
+            {
+                connection.Open();
+                using (SqlCommand selectCommand = new SqlCommand(selectStatement, connection))
+                {
+                    selectCommand.Parameters.AddWithValue("@username", username);
+                    selectCommand.Parameters.AddWithValue("@password", password);
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        Employee employee;
+                        if (reader.Read())
+                        {
+                            employee = new Employee
+                            {
+                                EmployeeID = (int)reader["id"],
+                                UserName = reader["username"].ToString(),
+                                Password = reader["password"].ToString(),
+                            };
+                            return employee;
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- Clinic/LoginForm.Designer.cs
+++ Clinic/LoginForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Clinic
 {
-    partial class Form1
+    partial class LoginForm
     {
         /// <summary>
         /// Required designer variable.
