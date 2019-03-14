f68718df9c7b6f7c402e53c155513313d89075c7
Chris Childress <cchildre@westga.edu>
2019-03-14 19:56:49 -0400

    created person class

---
+++ Clinic/Model/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic.Model
+{
+    /// <summary>
+    /// Represents person table with all information
+    /// </summary>
+    public abstract class Person
+    {
+        public int id { get; set; }
+        public string firstName {get; set;}
+        public string lastName { get; set; }
+        public DateTime dateOfBirth { get; set; }
+        public string socialSecurityNumber { get; set; }
+        public string gender { get; set; }
+        public string streetAddress { get; set; }
+        public string phone { get; set; }
+        public string zipcode { get; set; }
+
+    }
+}
