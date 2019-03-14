38fc70a28dd4ea52f2ce27df52f67c74ccec2d3a
Chris Childress <cchildre@westga.edu>
2019-03-14 19:58:17 -0400

    changed employee to extend person

--- Clinic/Model/Employee.cs
+++ Clinic/Model/Employee.cs
@@ -10,11 +10,9 @@ namespace Clinic.Model
     /// Represents an employee that can log into the system.
     /// Parent class of Nurse and admin
     /// </summary>
-    public class Employee
+    public class Employee : Person
     {
         public int EmployeeID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
 
