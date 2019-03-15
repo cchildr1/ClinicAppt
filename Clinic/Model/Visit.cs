443bdafa50aed6db6e92c84a76c2815a06adb35e
Chris Childress <cchildre@westga.edu>
2019-03-14 20:22:04 -0400

    added tests and visit classes

---
+++ Clinic/Model/Visit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic.Model
+{
+    /// <summary>
+    /// Represents a visit, contains the date and all biometric information
+    /// </summary>
+    public class Visit
+    {
+        public int id { get; set; }
+        public DateTime DateTime { get; set; }
+        public decimal Weight { get; set; }
+        public int BpSystolic { get; set; }
+        public int BpDiastolic { get; set; }
+        public decimal bodyTemperature { get; set; }
+        public int Pulse { get; set; }
+        public string Symptoms { get; set; }
+        public string Info { get; set; }
+        public Nurse Nurse { get; set; }
+        public Appointment Appointment { get; set; }
+        public string InitialDiagnosis { get; set; }
+        public string FinalDiagnosis { get; set; }
+        public List<Test> Tests { get; set; }
+    }
+}
