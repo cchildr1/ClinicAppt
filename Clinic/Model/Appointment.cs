451c7a41a6fbd02541ca932b2875c0b89955f282
Chris Childress <cchildre@westga.edu>
2019-03-14 20:16:00 -0400

    added patient class, changed appointment class to contain a doctor and a patient rather than their IDs

--- Clinic/Model/Appointment.cs
+++ Clinic/Model/Appointment.cs
@@ -11,8 +11,8 @@ namespace Clinic.Model
         public int AppointmentID { get; set; }
         public DateTime Scheduled_Date { get; set; }
         public string Reason_For_Visit { get; set; }
-        public int Doctor_ID { get; set; }
-        public int Patient_ID { get; set; }
+        public Doctor Doctor { get; set; }
+        public Patient Patient { get; set; }
         
     }
 }
---
+++ Clinic/Model/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic.Model
+{
+    /// <summary>
+    /// 
+    /// Represents a patient and contains the patient id, extends person
+    /// </summary>
+    public class Patient : Person
+    {
+        public int PatientID { get; set; }
+    }
+}
