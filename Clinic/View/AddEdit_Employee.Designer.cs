dbbd410d2ab61ba58886ebc9108b4e76741a2fb6
dcoleman90 <dcolem12@my.westga.edu>
2019-04-21 19:31:09 -0400

    Labels and textboxes are created for the Add_Edit_EmployeeInfo page

---
+++ Clinic/View/AddEdit_Employee.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Clinic.View
+{
+    partial class AddEdit_Employee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addEdit_employee_BASE_tablelayoutpanel = new System.Windows.Forms.TableLayoutPanel();
+            this.username_lbl = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.Label();
+            this.password_Confirmation_LBL = new System.Windows.Forms.Label();
+            this.username_textbox = new System.Windows.Forms.TextBox();
+            this.password_textbox = new System.Windows.Forms.TextBox();
+            this.password_conformation_textbox = new System.Windows.Forms.TextBox();
+            this.addEdit_employee_BASE_tablelayoutpanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // addEdit_employee_BASE_tablelayoutpanel
+            // 
+            this.addEdit_employee_BASE_tablelayoutpanel.ColumnCount = 2;
+            this.addEdit_employee_BASE_tablelayoutpanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.addEdit_employee_BASE_tablelayoutpanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.addEdit_employee_BASE_tablelayoutpanel.Controls.Add(this.password_conformation_textbox, 1, 2);
+            this.addEdit_employee_BASE_tablelayoutpanel.Controls.Add(this.password_textbox, 1, 1);
+            this.addEdit_employee_BASE_tablelayoutpanel.Controls.Add(this.username_lbl, 0, 0);
+            this.addEdit_employee_BASE_tablelayoutpanel.Controls.Add(this.password, 0, 1);
+            this.addEdit_employee_BASE_tablelayoutpanel.Controls.Add(this.password_Confirmation_LBL, 0, 2);
+            this.addEdit_employee_BASE_tablelayoutpanel.Controls.Add(this.username_textbox, 1, 0);
+            this.addEdit_employee_BASE_tablelayoutpanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.addEdit_employee_BASE_tablelayoutpanel.Location = new System.Drawing.Point(0, 0);
+            this.addEdit_employee_BASE_tablelayoutpanel.Name = "addEdit_employee_BASE_tablelayoutpanel";
+            this.addEdit_employee_BASE_tablelayoutpanel.RowCount = 5;
+            this.addEdit_employee_BASE_tablelayoutpanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.addEdit_employee_BASE_tablelayoutpanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.addEdit_employee_BASE_tablelayoutpanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.addEdit_employee_BASE_tablelayoutpanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.addEdit_employee_BASE_tablelayoutpanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.addEdit_employee_BASE_tablelayoutpanel.Size = new System.Drawing.Size(314, 450);
+            this.addEdit_employee_BASE_tablelayoutpanel.TabIndex = 0;
+            // 
+            // username_lbl
+            // 
+            this.username_lbl.AutoSize = true;
+            this.username_lbl.Location = new System.Drawing.Point(3, 0);
+            this.username_lbl.Name = "username_lbl";
+            this.username_lbl.Padding = new System.Windows.Forms.Padding(5);
+            this.username_lbl.Size = new System.Drawing.Size(65, 23);
+            this.username_lbl.TabIndex = 0;
+            this.username_lbl.Text = "Username";
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Location = new System.Drawing.Point(3, 90);
+            this.password.Name = "password";
+            this.password.Padding = new System.Windows.Forms.Padding(5);
+            this.password.Size = new System.Drawing.Size(63, 23);
+            this.password.TabIndex = 1;
+            this.password.Text = "Password";
+            // 
+            // password_Confirmation_LBL
+            // 
+            this.password_Confirmation_LBL.AutoSize = true;
+            this.password_Confirmation_LBL.Location = new System.Drawing.Point(3, 180);
+            this.password_Confirmation_LBL.Name = "password_Confirmation_LBL";
+            this.password_Confirmation_LBL.Padding = new System.Windows.Forms.Padding(5);
+            this.password_Confirmation_LBL.Size = new System.Drawing.Size(124, 23);
+            this.password_Confirmation_LBL.TabIndex = 2;
+            this.password_Confirmation_LBL.Text = "Password Confirmation";
+            // 
+            // username_textbox
+            // 
+            this.username_textbox.Location = new System.Drawing.Point(162, 5);
+            this.username_textbox.Margin = new System.Windows.Forms.Padding(5);
+            this.username_textbox.Name = "username_textbox";
+            this.username_textbox.Size = new System.Drawing.Size(133, 20);
+            this.username_textbox.TabIndex = 3;
+            // 
+            // password_textbox
+            // 
+            this.password_textbox.Location = new System.Drawing.Point(162, 95);
+            this.password_textbox.Margin = new System.Windows.Forms.Padding(5);
+            this.password_textbox.Name = "password_textbox";
+            this.password_textbox.Size = new System.Drawing.Size(133, 20);
+            this.password_textbox.TabIndex = 4;
+            // 
+            // password_conformation_textbox
+            // 
+            this.password_conformation_textbox.Location = new System.Drawing.Point(162, 185);
+            this.password_conformation_textbox.Margin = new System.Windows.Forms.Padding(5);
+            this.password_conformation_textbox.Name = "password_conformation_textbox";
+            this.password_conformation_textbox.Size = new System.Drawing.Size(133, 20);
+            this.password_conformation_textbox.TabIndex = 5;
+            // 
+            // AddEdit_Employee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 450);
+            this.Controls.Add(this.addEdit_employee_BASE_tablelayoutpanel);
+            this.Name = "AddEdit_Employee";
+            this.Text = "Edit Employee Info";
+            this.addEdit_employee_BASE_tablelayoutpanel.ResumeLayout(false);
+            this.addEdit_employee_BASE_tablelayoutpanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel addEdit_employee_BASE_tablelayoutpanel;
+        private System.Windows.Forms.Label username_lbl;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.Label password_Confirmation_LBL;
+        private System.Windows.Forms.TextBox password_conformation_textbox;
+        private System.Windows.Forms.TextBox password_textbox;
+        private System.Windows.Forms.TextBox username_textbox;
+    }
+}
--- Clinic/View/EditEmployeeInfo.cs
+++ Clinic/View/EditEmployeeInfo.cs
@@ -16,5 +16,10 @@ namespace Clinic.View
         {
             InitializeComponent();
         }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
