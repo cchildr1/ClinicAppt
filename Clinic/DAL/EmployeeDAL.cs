813ba471e552aea593d64de53f4434d6282e061f
Derrick Gregory <dgregor3@my.westga.edu>
2019-04-27 15:35:20 -0400

    Added unit test project to Clinic solution.

--- Clinic/DAL/EmployeeDAL.cs
+++ Clinic/DAL/EmployeeDAL.cs
@@ -11,24 +11,31 @@ namespace Clinic.DAL
 {
     class EmployeeDAL
     {
+        /// <summary>
+        /// Changes the employee credentails.
+        /// </summary>
+        /// <param name="edited_employee">the new employee object</param>
+        /// <param name="old_employee">the existing employee object</param>
+        /// <returns></returns>
         public bool EditEmployeeInfo(Employee edited_employee, Employee old_employee)
         {
             string updateStatement = "UPDATE users " +
                 "SET username = @new_username, " +
-                "password = @new_password " +
+                "password = HASHBYTES('SHA2_256', @new_password) " +
                 "WHERE username = @old_username AND " +
-           //     "password = @old_password AND " +
+                "password = HASHBYTES('SHA2_256', @old_password) AND " +
                 "id = @old_id AND " +
                 "person_id = @old_personID;";
+
             using (SqlConnection connection = ClinicDBConnection.GetConnection())
             {
                 connection.Open();
                 using (SqlCommand update = new SqlCommand(updateStatement, connection))
                 {
                     update.Parameters.AddWithValue("@new_username", edited_employee.UserName);
-                    update.Parameters.AddWithValue("new_password", Encoding.Default.GetBytes("qwerty")); //edited_employee.Password); CHANGED PASSWORDS STILL WORK AS TEST
+                    update.Parameters.AddWithValue("@new_password", edited_employee.Password); 
                     update.Parameters.AddWithValue("@old_username", old_employee.UserName);
-                    //     update.Parameters.AddWithValue("old_password", old_employee.Password);
+                    update.Parameters.AddWithValue("@old_password", old_employee.Password);
                     update.Parameters.AddWithValue("@old_id", old_employee.EmployeeID);
                     update.Parameters.AddWithValue("@old_personID", old_employee.PersonId);
                     int count = update.ExecuteNonQuery();
@@ -41,7 +48,7 @@ namespace Clinic.DAL
         /// <summary>
         /// This method allows users to add employee values to the database
         /// </summary>
-        /// <param name="addedEmployee"></param>
+        /// <param name="addedEmployee">The employee object to add to the db.</param>
         public void AddEmployee(Employee addedEmployee)
         {
             int employeeID = -1;
@@ -57,7 +64,7 @@ namespace Clinic.DAL
                     using (SqlCommand insertCommand = new SqlCommand(addEmployee, connection))
                     {
                         insertCommand.Parameters.AddWithValue("username", addedEmployee.UserName);
-                    insertCommand.Parameters.AddWithValue("password", Encoding.Default.GetBytes("qwerty"));//addedEmployee.Password)); New users Password is still 'test' WHY
+                        insertCommand.Parameters.AddWithValue("password", Encoding.Default.GetBytes("qwerty"));//addedEmployee.Password)); New users Password is still 'test' WHY
                         insertCommand.Parameters.AddWithValue("person_id", addedEmployee.PersonId);
 
                         insertCommand.ExecuteNonQuery();
@@ -78,8 +85,8 @@ namespace Clinic.DAL
         /// <summary>
         /// Returns the employeeID equal to the accepted personID
         /// </summary>
-        /// <param name="personID"></param>
-        /// <returns></returns>
+        /// <param name="personID">ID of the person</param>
+        /// <returns>Employee object</returns>
         public Employee GetEmployeeBy_PersonID(int personID)
         {
             Employee employee = new Employee();
@@ -101,7 +108,6 @@ namespace Clinic.DAL
                                 employee.PersonId = (int)reader["person_id"];
                                 employee.Password = reader["password"].ToString();
                                 employee.UserName = reader["username"].ToString();
-
                             }
                         }
                     }
---
+++ ClinicTests/TestUpdatingUserCredentials.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Clinic.Model;
+using Clinic.DAL;
+
+namespace ClinicTests
+{
+    [TestClass]
+    public class TestUpdatingUserCredentials
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
