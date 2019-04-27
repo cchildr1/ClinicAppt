db644acb8196b3f84036a949f5d743459c019009
Derrick Gregory <dgregor3@my.westga.edu>
2019-04-27 16:24:29 -0400

    Got two very simple tets to execute to test the EditEmployeeInfo method and both worked when password was modified.

--- ClinicTests/TestUpdatingUserCredentials.cs
+++ ClinicTests/TestUpdatingUserCredentials.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Clinic.Model;
 using Clinic.DAL;
+using Clinic.Controller;
 
 namespace ClinicTests
 {
@@ -9,9 +10,47 @@ namespace ClinicTests
     public class TestUpdatingUserCredentials
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetProperCreds()
         {
+            Employee oldEmployee = new Employee
+            {
+                UserName = "CharlieStrong",
+                Password = "password",
+                EmployeeID = 118,
+                PersonId = 147
+            };
+            Employee newEmployee = new Employee
+            {
+                UserName = "CharlieStrong",
+                Password = "password1",
+                EmployeeID = 118,
+                PersonId = 147
+            };
+            EmployeeController employeeController = new EmployeeController();
+            bool updated = employeeController.EditEmployeeInfo(newEmployee, oldEmployee);
+            Assert.IsTrue(updated);
+        }
 
+        [TestMethod]
+        public void GetImproperCreds()
+        {
+            Employee oldEmployee = new Employee
+            {
+                UserName = "CharlieStrong",
+                Password = "gobbledeegook",
+                EmployeeID = 118,
+                PersonId = 147
+            };
+            Employee newEmployee = new Employee
+            {
+                UserName = "CharlieStrong",
+                Password = "fhhfhffhfh",
+                EmployeeID = 118,
+                PersonId = 147
+            };
+            EmployeeController employeeController = new EmployeeController();
+            bool updated = employeeController.EditEmployeeInfo(newEmployee, oldEmployee);
+            Assert.IsFalse(updated);
         }
     }
 }
